f3d7729f7435ba3582bc9950d50afb3370df2ef1
support@myeffecto.com <support@myeffecto.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-25 11:10:13 +0000

    new update
    git-svn-id: https://plugins.svn.wordpress.org/instant-feedback/trunk@778284 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- MyEffecto.php
+++ MyEffecto.php
@@ -6,112 +6,105 @@ Description: Getting customized and interactive feedback for your blog
 Version: 1.0
 Author URI: www.myeffecto.com
 */
-	require('DBFunctions.php');
-	require('PostConfiguration.php');
+error_reporting(0);
+require('DBFunctions.php');
+require('PostConfiguration.php');
+/* Add MyEffecto link to Setting Tab. */
+add_action('admin_menu', 'myeffecto_admin_actions');
+add_filter( 'the_content', 'echoEndUserPlugin');
 
-	/* Add MyEffecto link to Setting Tab. */
-	add_action('admin_menu', 'myeffecto_admin_actions');
+$embedCode = null;
 
-	add_filter( 'the_content', 'echoEndUserPlugin');
-	$embedCode = null;
+/* Show plugin on Menu bar */
+function myeffecto_admin_actions() {
+	add_options_page('MyEffecto', 'MyEffecto', 'manage_options', _FILE_, 'myeffecto_admin', null, '59.5');
+}
 
-	/* Show plugin on Menu bar */
-	function myeffecto_admin_actions() {
-		add_options_page('MyEffecto', 'MyEffecto', 'manage_options', _FILE_, 'myeffecto_admin', null, '59.5');
-	}
+wp_enqueue_script("jquery");
+wp_enqueue_script("jquery-ui-dialog");
+wp_enqueue_style("wp-Myeffecto", "http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css");
 
-	wp_enqueue_script("jquery");
-	wp_enqueue_script("jquery-ui-dialog");
-	wp_enqueue_style("sdfsdfsf", "http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css");
+$shortname = $_GET['shortname'];
+$globalPostID = $_GET['postID'];
 
-	$shortname = $_GET['shortname'];
-	$globalPostID = $_GET['postID'];
-	function myeffecto_admin() {
-		 $user_id = get_current_user_id();
-		 $data=$_POST['dataToSend'];
+function myeffecto_admin() {
+	 $user_id = get_current_user_id();
+	 $data=$_POST['dataToSend'];
 
-		 $postID = $_GET['postID'];
-		 $postName = $_GET['postName'];
-		 $postURL = $_GET['postURL'];
-		 $shortname = $_GET['shortname'];
-	?>
-		 <form id="submitForm" action="" method="post" style="display:none;"><input name="isToInsert" value="true" id="isToInsert" type="hidden"/><input name="dataToSend" id="dataToSend" type="hidden"/><input type='submit'/></form>
-		 <form id="reloadForm" action="" method="post" style="display:none;"><input type='submit'/></form>
-	<?php
-
-		if(isset($data)) {
-			$isCodeExist = getEmbedCodeByPostID($postID);
-			if ($isCodeExist == null) {
-				if (!isset($postID)) {
-					$defaultEdit = $_GET['pluginType'];
-					if (isset($defaultEdit) && $defaultEdit == "defaultEdit") {
-						updateEmbedCode($data, 0);
-					?>
-							<script type="text/javascript">
-						   <!--
-						      	window.location= <?php echo "'" . $postURL . "&action=edit&plugin=success'"; ?>;
-						   //-->
-						   </script>
-					<?php
-						} else {
-						insertInDb($user_id, null, $data, null);
-						if (isset($postURL)) {
-							?>
-								<script type="text/javascript">
-							   <!--
-							      	window.location= <?php echo "'" . $postURL . "&action=edit&plugin=success'"; ?>;
-							   //-->
-							   </script>
-							<?php
-							//header("Location:$postURL&action=edit&plugin=success");
-						} else {
-							echo "<h1><center>Your emoticon set has been added on your posts successfully.<br><br>Go to your post to see the effect.</center></h1>";
-						}
-					}
-				} else {
-					insertInDb($user_id, null, $data, $postID);
-					?>
+	 $postID = $_GET['postID'];
+	 $postName = $_GET['postName'];
+	 $postURL = $_GET['postURL'];
+	 $shortname = $_GET['shortname'];
+	 
+?><form id="submitForm" action="" method="post" style="display:none;"><input name="isToInsert" value="true" id="isToInsert" type="hidden"/><input name="dataToSend" id="dataToSend" type="hidden"/><input type='submit'/></form>
+  <form id="reloadForm" action="" method="post" style="display:none;"><input type='submit'/></form>
+<?php
+	if(isset($data)) {
+		$isCodeExist = getEmbedCodeByPostID($postID);
+		if ($isCodeExist == null) {
+			if (!isset($postID)) {
+				$defaultEdit = $_GET['pluginType'];
+				if (isset($defaultEdit) && $defaultEdit == "defaultEdit") {
+					updateEmbedCode($data, 0);
+				?>
 						<script type="text/javascript">
-					   <!--
-					      	window.location= <?php echo "'" . $postURL . "&action=edit&plugin=success'"; ?>;
-					   //-->
-					   </script>
-					<?php
-					// header("Location:$postURL&action=edit&plugin=success");
+					   <!--
+							window.location= <?php echo "'" . $postURL . "&action=edit&plugin=success'"; ?>;
+					   //-->
+					   </script>
+				<?php
+					} else {
+					insertInDb($user_id, null, $data, null);
+					if (isset($postURL)) {
+						?>
+							<script type="text/javascript">
+						   <!--
+								window.location= <?php echo "'" . $postURL . "&action=edit&plugin=success'"; ?>;
+						   //-->
+						   </script>
+						<?php
+					} else {
+						echo "<h1><center>Your emoticon set has been added on your posts successfully.<br><br>Go to your post to see the effect.</center></h1>";
+					}
 				}
 			} else {
-				$addType = $_GET['pluginType'];
-				if ($addType == "postEdit") {
-					updateEmbedCode($data, $postID);
-					?>
-						<script type="text/javascript">
-							window.location= <?php echo "'" . $postURL . "&action=edit&plugin=success'"; ?>;
-						</script>
-					<?php
-					// header("Location:$postURL&action=edit&plugin=success");
-				}
+				insertInDb($user_id, null, $data, $postID);
+				?>
+					<script type="text/javascript">
+				   <!--
+						window.location= <?php echo "'" . $postURL . "&action=edit&plugin=success'"; ?>;
+				   //-->
+				   </script>
+				<?php
 			}
-		 } else {
-	?>
-<?php
-	$image=plugins_url( 'loading.gif' , __FILE__ );
-	//echo $image;
-	?>
- <style type="text/css">
-		#load {
-			background: url(" <?php echo $image;?> ") no-repeat scroll center center #FFF;
-			bottom: 0;
-			left: 0;
-			position: absolute;
-			opacity: 0.63;
-			right: 0;
-			top: 0;
-			width: 100%;
-			z-index: 1000;
+		} else {
+			$addType = $_GET['pluginType'];
+			if ($addType == "postEdit") {
+				updateEmbedCode($data, $postID);
+				?>
+					<script type="text/javascript">
+						window.location= <?php echo "'" . $postURL . "&action=edit&plugin=success'"; ?>;
+					</script>
+				<?php
+			}
 		}
+	 } else {
+?>
+<style type="text/css">
+	#load {
+		background: url(" <?php echo plugins_url( 'loading.gif' , __FILE__ );?> ") no-repeat scroll center center #FFF;
+		bottom: 0;
+		left: 0;
+		position: absolute;
+		opacity: 0.63;
+		right: 0;
+		top: 0;
+		width: 100%;
+		z-index: 1000;
+	}
 </style>
 
-		<div class="wrap" style="overflow-x : hidden;">
+		<div class="wrap" style="overflow-x : hidden; position : relative;">
 			<h2>MyEffecto Admin</h2>
 	<?php
 			global $embedCode;
@@ -158,16 +151,70 @@ Author URI: www.myeffecto.com
 	<?php
 		}
 	}
+	
+	function configurationScript($shortname, $globalPostID) {
+		echo '<script>
+				var shortname = "'.$shortname.'";
+				var effecto_identifier = "'.$globalPostID.'";
+
+				function save(shortname) {
+					jQuery(\'#load\').show();
+					/* if (shortname == null || shortname === "" || shortname === "undefined") {
+						ifrm.contentWindow.postMessage("Save","http://www.myeffecto.com");
+					} else {
+						ifrm.contentWindow.postMessage("Save#~#delete#~#"+shortname+"#~#"+effecto_identifier,"http://www.myeffecto.com");
+						shortname = "";
+						effecto_identifier = "";
+					} */
+				}
+
+				function receiveMessage(event) {
+					var rcvdMsg = event.data;
+					var msg = rcvdMsg.split("#~#");
+
+					if (msg[0] == "save") {
+						postIframeCode(msg[1]);
+					} else if(msg[0] == "loggedIn") {
+						afterLoginSuccess();
+					} else if (msg[0] == "error") {
+						alert("Error occured");
+					} else if (msg[0] == "pluginLoggedIn") {
+						showButtonCode(shortname);
+					} /*else if(msg[0] == "apiKey") {
+						addKey(msg[1]);
+					}*/
+				}
+
+				function postIframeCode(rcvdMsg) {
+					var dataToSend = { "insert":"true", "data" : rcvdMsg};
+
+					jQuery("#dataToSend").val(rcvdMsg);
+					jQuery(\'#submitForm\').submit();
+				}
+
+				function showButtonCode(shortname) {
+					jQuery(\'#generate\').remove();
+					if (shortname === null) {
+						jQuery(\'#effectoFrame\').after(jQuery(\'<center><h3><input type="button" id="generate" onclick="save("")" value="Apply Emotion Set" style="font-size : 22px; padding-top : 7px; padding-bottom : 30px;" class="button-primary" /></h3></center>\'));
+					} else {
+						jQuery(\'#effectoFrame\').after(jQuery(\'<center><h3><input type="button" id="generate" onclick="save(\' + shortname + \')" value="Apply Emotion Set" style="font-size : 22px; padding-top : 7px; padding-bottom : 30px;" class="button-primary" /></h3></center>\'));
+					}
+				}
+
+				function afterLoginSuccess() {
+					jQuery(\'#effectoFrame\').parent().prepend(jQuery(\'<input type="button" id="generate" onclick="save("")" value="Generate Plugin" class="button-primary"/>\'));
+					ifrm.setAttribute("src", "http://www.myeffecto.com/configureplug");
+				}';
+	}
 
 	function echoFirstUserScript() {
-		global $postName;
-		echo '
-	
-			 <script type="text/javascript">
-				var ifrm = null;
+		global $shortname;
+		global $globalPostID;
+		configurationScript($shortname, $globalPostID);
+		echo '	var ifrm = null;
 				window.onload=function(){
 					ifrm = document.getElementById("effectoFrame");
-					ifrm.setAttribute("src", "http://www.myeffecto.com/login?callback=configureplug&postName='.$postName.'");
+					ifrm.setAttribute("src", "http://www.myeffecto.com/login?callback=configureplug");
 					ifrm.setAttribute("frameborder","0");
 					ifrm.setAttribute("allowtransparency","true");
 
@@ -177,36 +224,29 @@ Author URI: www.myeffecto.com
 				};
 			</script>
 			<div id="load" style="display:none;"></div>
-			<iframe id="effectoFrame" src ="http://www.myeffecto.com/login?callback=configureplug&postName='.$postName.'" width="100%" height="465">';
+			<iframe id="effectoFrame" src ="http://www.myeffecto.com/login?callback=configureplug" width="100%" height="465">';
 	}
 
 	function echoUserScript() {
-		global $postName;
-		echo '	
-				
-				<script type="text/javascript">
-					var ifrm= null;
-					window.onload=function(){
-						ifrm = document.getElementById("effectoFrame");
-						ifrm.setAttribute("src", "http://www.myeffecto.com/login?callback=configureplug&postName='.$postName.'");
-						ifrm.setAttribute("frameborder","0");
-						ifrm.setAttribute("allowtransparency","true");
+		global $shortname;
+		global $globalPostID;
+		configurationScript($shortname, $globalPostID);
+		echo '	var ifrm= null;
+				window.onload=function(){
+					ifrm = document.getElementById("effectoFrame");
+					ifrm.setAttribute("src", "http://www.myeffecto.com/login?callback=configureplug");
+					ifrm.setAttribute("frameborder","0");
+					ifrm.setAttribute("allowtransparency","true");
 
-						ifrm.style.width = "100%";
-						ifrm.style.height = "465";
-						window.addEventListener("message", receiveMessage, false);
-					};
-				</script>
-				<div id="load" style="display:none;"></div>
-				<iframe id="effectoFrame" src ="http://www.myeffecto.com/login?callback=configureplug&postName='.$postName.'" width="100%" height="465"/>';
+					ifrm.style.width = "100%";
+					ifrm.style.height = "465";
+					window.addEventListener("message", receiveMessage, false);
+				};
+			</script>
+			<div id="load" style="display:none;"></div>
+			<iframe id="effectoFrame" src ="http://www.myeffecto.com/login?callback=configureplug" width="100%" height="465"/>';
 	}
-
-	/* Simple string replace function */
-	function replaceText ($text) {
-		$text = str_replace('\"','', $text);
-		return $text;
-	}
-
+	
 	/* Show plugin in posts. */
 	function echoEndUserPlugin($text) {
 		$postId = get_the_ID();
@@ -217,7 +257,6 @@ Author URI: www.myeffecto.com
 		if ($apiEmbedArray == null) {
 			$apiEmbedArray = getEmbedCodeByPostID(0);
 		}
-
 		if (strpos($_SERVER['REQUEST_URI'],'?p=') !== false) {
 			$apiEmbedArray = str_replace("var effectoPostId=''","var effectoPostId='".$postId."'", $apiEmbedArray);
 			$apiEmbedArray = str_replace("var effectoPreview=''","var effectoPreview='false'", $apiEmbedArray);
@@ -230,6 +269,12 @@ Author URI: www.myeffecto.com
 		} else {
 			return $text;
 		}
+	}
+
+	/* Simple string replace function */
+	function replaceText ($text) {
+		$text = str_replace('\"','', $text);
+		return $text;
 	}
 
 	function addAlert($pluginStatus) { 
@@ -254,81 +299,3 @@ Author URI: www.myeffecto.com
 	}
 	register_uninstall_hook( __FILE__, 'pluginUninstall' );
 ?>
-
-<script type="text/javascript">
-	<?php global $shortname; ?>
-	var shortname = "<?php echo $shortname; ?>";
-	var effecto_identifier = "<?php echo $globalPostID; ?>";
-	
-	function save(shortname) {
-		jQuery('#load').show();
-		if (shortname == null || shortname === "" || shortname === "undefined") {
-			ifrm.contentWindow.postMessage("Save","http://www.myeffecto.com");
-		} else {
-			ifrm.contentWindow.postMessage("Save#~#delete#~#"+shortname+"#~#"+effecto_identifier,"http://www.myeffecto.com");
-			shortname = "";
-			effecto_identifier = "";
-		}
-	}
-
-	function receiveMessage(event) {
-		var rcvdMsg = event.data;
-		var msg = rcvdMsg.split("#~#");
-
-		if (msg[0] == "save") {
-			postIframeCode(msg[1]);
-		} else if(msg[0] == "loggedIn") {
-			afterLoginSuccess();
-		} else if (msg[0] == "error") {
-			alert("Error occured");
-		} else if (msg[0] == "pluginLoggedIn") {
-			showButtonCode(shortname);
-			//jQuery('body').fadeOut(2000);
-		} /*else if(msg[0] == "apiKey") {
-			addKey(msg[1]);
-		}*/
-	}
-
-	function postIframeCode(rcvdMsg) {
-		var dataToSend = { "insert":"true", "data" : rcvdMsg};
-
-		jQuery("#dataToSend").val(rcvdMsg);
-		jQuery('#submitForm').submit();
-	}
-
-	function showButtonCode(shortname) {
-		jQuery('#generate').remove();
-		if (shortname === null) {
-			jQuery('#effectoFrame').after(jQuery('<center><h3><input type="button" id="generate" onclick="save("")" value="Apply Emotion Set" style="font-size : 22px; padding-top : 7px; padding-bottom : 30px;" class="button-primary" /></h3></center>'));
-		} else {
-			jQuery('#effectoFrame').after(jQuery('<center><h3><input type="button" id="generate" onclick="save(' + shortname + ')" value="Apply Emotion Set" style="font-size : 22px; padding-top : 7px; padding-bottom : 30px;" class="button-primary" /></h3></center>'));
-		}
-	}
-
-	function afterLoginSuccess() {
-		jQuery('#effectoFrame').parent().prepend(jQuery('<input type="button" id="generate" onclick="save("")" value="Generate Plugin" class="button-primary"/>'));
-		ifrm.setAttribute("src", "http://www.myeffecto.com/configureplug");
-	}
-
-	
-	function addKey(key)
-	{
-		//var a = document.getElementById("kk").value;
-
-		jQuery.ajax({
-			type : 'get',
-			url : "<?php get_site_url(); ?>/wp-content/plugins/changeVar.php?apiKey=" + key,
-			success : function(data){
-				//window.location.reload(true);
-			},
-			error : function(data){
-				alert(data);
-			}
-		});
-	}
-	
-	function test() {
-		alert("a;kdjasidjalsjdlka;sdjlkas");
-	}
-
-</script>
