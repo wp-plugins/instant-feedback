f5311e9880cbd690e2e16e9c6e7507a12431956a
support@myeffecto.com <support@myeffecto.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-30 05:57:06 +0000

    revert back
    git-svn-id: https://plugins.svn.wordpress.org/instant-feedback/trunk@957150 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- MyEffecto.php
+++ MyEffecto.php
@@ -3,7 +3,7 @@
 Plugin Name: My Effecto
 Plugin URI: www.myeffecto.com
 Description: Getting customized and interactive feedback for your blog.
-Version: 1.0.26
+Version: 1.0.25
 Author URI: www.myeffecto.com
 */
 
